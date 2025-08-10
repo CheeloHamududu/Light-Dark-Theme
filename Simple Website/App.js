7e550e43f0ffa5d668ff3b1e4a11734d8db89dca
Cheelo <112832809+CheeloHamududu@users.noreply.github.com>
2025-08-10 12:18:20 +0200

    Updated app.js

--- Simple Website/App.js
+++ Simple Website/App.js
@@ -7,14 +7,14 @@ function switchTheme() {
 }
 
 switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
-
-    const className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
-    } else {
-        this.textContent = "Light";
-    }
+  if (document.body.classList.contains('light-theme')) {
+    document.body.classList.remove('light-theme');
+    document.body.classList.add('dark-theme');
+    this.textContent = "Light";
+  } else {
+    document.body.classList.remove('dark-theme');
+    document.body.classList.add('light-theme');
+    this.textContent = "Dark";
+  }
 });
 
